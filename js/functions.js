97041c8391dda42147a12b7d38fd8791d3e19fa7
Victoria <vbalzhieva@gmail.com>
2023-11-04 03:03:44 +0300

    доп задание

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ function checkStringLength(string, maxLength) {
   return string.length <= maxLength;
 }
 
-checkStringLength();
+checkStringLength('ssss', 10);
 
 function isPalindrome(string) {
   const normalizedString = string.replaceAll(' ', '').toLowerCase();
@@ -26,4 +26,23 @@ function detachNumbers(string) {
   return parseInt(number, 10);
 }
 
-detachNumbers();
+detachNumbers('2023 year');
+
+function checkMeetingDuration (dayStart, dayEnd, meetingStart, meetingDuration) {
+  dayStart = dayStart.split(':');
+  dayEnd = dayEnd.split(':');
+  meetingStart = meetingStart.split(':');
+  const meetingEnd = meetingStart[0]*60 + +meetingStart[1] + meetingDuration;
+
+  if (dayStart[0] > meetingStart[0]) {
+    return false;
+  }
+
+  return dayEnd[0]*60 + +dayEnd[1] >= meetingEnd;
+}
+
+checkMeetingDuration('08:00', '17:30', '14:00', 90);
+checkMeetingDuration('8:0', '10:0', '7:0', 120);
+checkMeetingDuration('08:00', '14:30', '14:00', 90);
+checkMeetingDuration('14:00', '17:30', '08:00', 90);
+checkMeetingDuration('08:00', '17:30', '08:00', 900);
